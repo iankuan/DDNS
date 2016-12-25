c7a94567d6eefdb4f9b61e004cd80ff0311e3010
c14006078 <c14006078@gmail.com>
2016-12-25 16:07:00 +0800

    Init the limit file for configuration.

---
+++ config_limit.h
@@ -0,0 +1,15 @@
+/** 
+ * 
+ * labels          63 octets or less
+ * 
+ * names           255 octets or less
+ * 
+ * TTL             positive values of a signed 32 bit number.
+ * 
+ * UDP messages    512 octets or less
+*/
+
+#define LABEL_LIMIT  63
+#define NAME_LIMIT  255
+#define TTL_LIMIT    32
+#define UDP_LIMIT   512
