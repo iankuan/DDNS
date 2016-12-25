9a9fe21921970d0d32a80e94cfbfe4de49553627
c14006078 <c14006078@gmail.com>
2016-12-25 16:05:12 +0800

    Init Macro.h file for MAX and MIN that kind of functions.

---
+++ macro.h
@@ -0,0 +1,10 @@
+#define MIN(a,b) ({\
+                    typeof(a) _a = a; \
+                    typeof(b) _b = b; \
+                    _a < _b ? _a : _b;})
+
+#define MAX(a,b) ({\
+                    typeof(a) _a = a; \
+                    typeof(b) _b = b; \
+                    _a > _b ? _a : _b;})
+                
