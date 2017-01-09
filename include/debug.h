8118546990aa333c0e81f71e7bcbcc7dedcec06f
c14006078 <c14006078@gmail.com>
2017-01-09 14:13:08 +0800

    New Debug macro, we can control debug output.

---
+++ include/debug.h
@@ -0,0 +1,32 @@
+/**
+ * @file This file is for Debug, we can use this file like logcat
+ */
+
+/**
+ * error detect for unix command
+ */
+#define syserr(variable, function) \
+    if (variable) { \
+        perror(function); \
+        exit(EXIT_FAILURE); \
+    }
+
+/**
+ * Define own log and debug output.
+ *
+ * "__VA_ARG__" is varible MACRO substitue. It can make MACRO printf
+ * include formats and arguments.
+ */
+#ifdef DEBUG
+#define dprintf( ...) printf("DEBUG: " __VA_ARGS__)
+#define eprintf( ...) \
+    do { \
+        fprintf( stderr, "ERROR: "); fprintf( stderr, __VA_ARGS__); \
+    } while (0)
+#else
+#define dprintf( ...)
+#define eprintf( ...) \
+    do { \
+        fprintf(stderr, "ERROR: "); fprintf( stderr, __VA_ARGS__); \
+    } while (0)
+#endif
